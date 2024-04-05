efe33275899b7b8d14c6933d85a25cc70e0cf952
BinhQuocLy <lyquocbinh02@gmail.com>
2024-04-05 18:18:28 +0700

    Update naming convention

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "web_dns" {
+output "private_web_dns" {
   value       = module.ec2.web_alb_dns
   description = "Web public DNS"
 }
 
-output "app_dns" {
+output "public_app_dns" {
   value       = "app.test.com"
   description = "App private DNS"
 }
